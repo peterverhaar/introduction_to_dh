694ba3781eedd00fbb88c739ff0526e5625aac51
peterverhaar <peter.verhaar@gmail.com>
2022-03-18 09:06:42 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,6 +1,8 @@
 urls_dict = { 'https://edu.nl/h6khn':'Visualise_tweets.ipynb',
         'https://edu.nl/mkgtu':'tweets.tsv',
-        'https://edu.nl/he7hh':'hash_tags.csv' }
+        'https://edu.nl/he7hh':'hash_tags.csv' , 
+        'https://edu.nl/bcm4x':'bli.csv' , 
+           'https://edu.nl/p97ma':'pisa.csv' }
 
 import requests
 
@@ -9,3 +11,4 @@ for link in urls_dict:
     if response.status_code == 200:
         with open( urls_dict[link] , 'w' , encoding = 'utf8' ) as fh:
             fh.write( response.text )
+        
