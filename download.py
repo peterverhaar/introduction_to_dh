b321414b9009c2f8ec39d5fdbdfc830eec4ee4a5
peterverhaar <peter.verhaar@gmail.com>
2022-07-11 14:09:02 +0200

    Update download.py

--- download.py
+++ download.py
@@ -1,3 +1,5 @@
+# https://eur03.safelinks.protection.outlook.com/?url=https%3A%2F%2Ffilesender.surf.nl%2F%3Fs%3Ddownload%26token%3D262afbf5-db16-4e61-b275-05cca192fa14&data=05%7C01%7CP.A.F.Verhaar%40library.leidenuniv.nl%7C0c0b5c43f51c4f312cde08da6335ae69%7Cca2a7f76dbd74ec091086b3d524fb7c8%7C0%7C0%7C637931379465346283%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=zJMpQWlWIBPtrRfQHht7BWlV9JWGRuNGav8VpME0o%2F0%3D&reserved=0
+
 urls_dict = { 'https://raw.githubusercontent.com/peterverhaar/python_tutorial/main/12%20Visualisation.ipynb':'12 Visualisation.ipynb',
  'https://raw.githubusercontent.com/peterverhaar/twitter_tutorial/main/Twitter.ipynb':'Twitter.ipynb' }
 
